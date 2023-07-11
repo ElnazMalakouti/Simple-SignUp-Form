97f80e72de5a87cab39a90e6314a93d415c88cab
Elnaz <elnazmalakouti@gmail.com>
2023-07-12 01:04:37 +0330

    test email regex

--- src/Components/SignUpForm/index.jsx
+++ src/Components/SignUpForm/index.jsx
@@ -1,6 +1,23 @@
+import { useState } from "react"
 import Button from "../Button"
 
 const SignUpForm = () => {
+
+    const [emailIsValid, setEmailIsValid] = useState(true)
+
+    const [emailInputValue , setEmailInputValue] = useState('')
+
+    function validateEmail(email) {
+
+        const emailRegex =  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+        const isValidEmail = emailRegex.test(email);
+
+        setEmailIsValid(isValidEmail)
+
+        console.log(`emailRegex: ${emailIsValid}`)
+    }
+
     return (
         <>
             <div className="w-full md:w-[90%] lg:w-[80%] xl:w-[60%] h-full md:h-[90%] lg:h-[80%] rounded-3xl bg-white md:py-4 md:px-2 flex flex-col md:flex-row gap-8">
@@ -36,11 +53,21 @@ const SignUpForm = () => {
                     </div>
 
                     <div className="w-full flex flex-col gap-1 text-xs ">
-                        <label className="font-[Roboto-Bold]">Email Address</label>
-                        <input className="w-full h-12 px-4 rounded-lg border-2 hover:border-[#292731] hover:text-[#292731]" placeholder="email@company.com"/>
+                        <div className="w-full flex justify-between items-center">
+                            <label className="font-[Roboto-Bold]">Email Address</label>
+                            <p className={`${emailIsValid ? 'hidden' : 'block'} text-red-500`}>Valid email required</p>
+                        </div>
+                        <input
+                            type="email"
+                            value={emailInputValue}
+                            onChange={(e) => setEmailInputValue(e.target.value)}
+                            className={`w-full h-12 px-4 rounded-lg border focus:outline-0 ${emailIsValid ? 'border-2 hover:border-[#292731] hover:text-[#292731]' : 'border-1 border-red-500 bg-red-100 text-red-500'}`}
+                            placeholder="email@company.com" />
                     </div>
 
-                    <Button>Subscribe to mountly newsletter</Button>
+                    <Button onClick={() => validateEmail(emailInputValue)}>
+                        Subscribe to mountly newsletter
+                    </Button>
 
 
                 </div>
